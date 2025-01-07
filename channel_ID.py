5ba2e6be8d31b3eeeef6e0defdf01375d8801f2e
Sourav Kumar Sheel <souravkrsheel99@gmail.com>
2025-01-08 01:52:19 +0530

    First Commit

---
+++ channel_ID.py
@@ -0,0 +1,24 @@
+### Channel ID list for YouTube channels of 10 Indian and 10 foreign YouTube channels.
+
+CHANNEL_ID = [
+    'UCNU_lfiiWBdtULKOw6X0Dig',
+    'UCh3RpsDV8pS_fXJ6NBX4fhQ',
+    'UCJjX28Z_JfCIBOPHsC-0MnQ',
+    'UCuFz9ejz2Bu-jfKuJlErbEA',
+    'UCkw4JCwteGrDHIsyIIKo4tQ',
+    'UCsvqVGtbbyHaMoevxPAq9Fg',
+    'UC2sYit3cZ2vRw6RNgoNRXUw',
+    'UCdXmj3pLsxH2D0ybzJZvnHw',
+    'UCeVMnSShP_Iviwkknt83cww',
+    'UCtYLUTtgS3k1Fg4y5tAhLbw',
+    'UCYO_jab_esuFRV4b17AJtAw',
+    'UCV0qA-eDDICsRR9rPcnG7tw',
+    'UCxX9wt5FWQUAAz4UrysqK9A',
+    'UCbfYPyITQ-7l4upoX8nvctg',
+    'UCfzlCWGWYyIQ0aLC5w48gBQ',
+    'UCCezIgC97PvUuR4_gbFUs5g',
+    'UCWN3xxRkmTPmbKwht9FuE5A',
+    'UCiT9RITQ9PW6BhXK0y2jaeg',
+    'UCV8e2g4IWQqK71bbzGDEI4Q',
+    'UCFBoqaPTCtGJi8kr7pV33Tg'
+]
